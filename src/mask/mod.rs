cdfe7a70bb112aa821a8b33a3b4cba70deaefa7d
Markus Ineichen <kusi.ineichen@gmail.com>
2025-01-07 13:12:02 +0100

    Implement ordered subgroups

--- src/mask/mod.rs
+++ src/mask/mod.rs
@@ -1,4 +1,4 @@
-use std::num::NonZeroU16;
+use std::{collections::BinaryHeap, num::NonZeroU16};
 
 use eframe::egui::{
     self, load::SizedTexture, Color32, ColorImage, TextureHandle, TextureOptions, Ui,
@@ -62,10 +62,12 @@ impl MaskImage {
 
             let mut pixels = vec![Color32::TRANSPARENT; self.size[0] * self.size[1]];
 
-            for (pos, len) in self.subgroups() {
-                let group_color = Color32::from_rgba_premultiplied(64, 64, 0, 64);
-                let pos = pos as usize;
-                pixels[pos..(pos + len.get() as usize)].fill(group_color);
+            for (_group_id, subgroups) in self.subgroups() {
+                for &(pos, len) in subgroups {
+                    let group_color = Color32::from_rgba_premultiplied(64, 64, 0, 64);
+                    let pos = pos as usize;
+                    pixels[pos..(pos + len.get() as usize)].fill(group_color);
+                }
             }
 
             let handle = ui.ctx().load_texture(
@@ -83,12 +85,102 @@ impl MaskImage {
             None
         }
     }
-    fn subgroups(&self) -> impl Iterator<Item = (u32, NonZeroU16)> + '_ {
+
+    fn subgroups(&self) -> impl Iterator<Item = (usize, &Vec<(u32, NonZeroU16)>)> {
         self.annotations
             .0
             .iter()
-            .flat_map(|(_, b)| b)
-            .chain(self.history.iter().flatten())
-            .copied()
+            .map(|(_, b)| b)
+            .chain(self.history.iter())
+            .enumerate()
+    }
+
+    fn subgroups_ordered(&self) -> impl Iterator<Item = (usize, u32, NonZeroU16)> + '_ {
+        struct HeapItem<T>((u32, NonZeroU16), usize, T);
+
+        impl<T> Eq for HeapItem<T> {}
+        impl<T> PartialEq for HeapItem<T> {
+            fn eq(&self, other: &Self) -> bool {
+                self.0 == other.0
+            }
+        }
+        impl<T> PartialOrd for HeapItem<T> {
+            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+                self.0 .0.partial_cmp(&other.0 .0).map(|x| x.reverse())
+            }
+        }
+        impl<T> Ord for HeapItem<T> {
+            fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+                self.0 .0.cmp(&other.0 .0).reverse()
+            }
+        }
+
+        struct GroupIterator<'a>(BinaryHeap<HeapItem<std::slice::Iter<'a, (u32, NonZeroU16)>>>);
+
+        let x: BinaryHeap<_> = self
+            .subgroups()
+            .map(|(group_id, x)| {
+                let mut iter = x.iter();
+                HeapItem(
+                    *iter.next().expect("No empty groups available"),
+                    group_id,
+                    iter,
+                )
+            })
+            .collect();
+
+        impl<'a> Iterator for GroupIterator<'a> {
+            type Item = (usize, u32, NonZeroU16);
+
+            fn next(&mut self) -> Option<Self::Item> {
+                if let Some(HeapItem((pos, len), group_id, mut rest)) = self.0.pop() {
+                    if let Some(x) = rest.next() {
+                        self.0.push(HeapItem(*x, group_id, rest));
+                    }
+                    Some((group_id, pos, len))
+                } else {
+                    None
+                }
+            }
+        }
+        GroupIterator(x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn iter_sorted() {
+        let mut history = History::default();
+        history.push((
+            "Foo".into(),
+            vec![
+                (22, NonZeroU16::try_from(7).unwrap()),
+                (39, NonZeroU16::try_from(1).unwrap()),
+                (42, NonZeroU16::try_from(7).unwrap()),
+            ],
+        ));
+        let x = MaskImage {
+            size: [10, 10],
+            annotations: Annotations(vec![
+                (
+                    "Test".into(),
+                    vec![
+                        (2, NonZeroU16::try_from(5).unwrap()),
+                        (12, NonZeroU16::try_from(5).unwrap()),
+                    ],
+                ),
+                ("Test2".into(), vec![(32, NonZeroU16::try_from(5).unwrap())]),
+            ]),
+            history,
+            texture_handle: None,
+        };
+        let group_sequence: Vec<_> = x
+            .subgroups_ordered()
+            .map(|(group_id, _, _)| group_id)
+            .collect();
+        assert_eq!(group_sequence, vec![0, 0, 2, 1, 2, 2]);
     }
 }
