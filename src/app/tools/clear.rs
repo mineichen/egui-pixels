44656da0d81501cc607c4010cfe69449c6ab8b86
Markus Ineichen <kusi.ineichen@gmail.com>
2025-01-16 14:46:48 +0100

    Allow clearing single pixels without drag

--- src/app/tools/clear.rs
+++ src/app/tools/clear.rs
@@ -9,17 +9,12 @@ impl ImageViewerApp {
         cursor_image_pos: (usize, usize),
         ctx: &egui::Context,
     ) {
-        if let (ImageState::Loaded(ImageStateLoaded { masks, .. }), Some(region)) = (
-            &mut self.image_state,
-            self.tools.drag_stopped(cursor_image_pos, &response, ctx),
-        ) {
-            masks.clear_region(region);
-
-            // if let Some((_, _, loaded)) = self.selector.current() {
-            //     *loaded = true;
-            // } else {
-            //     warn!("Couldn't mark URL as containing masks")
-            // }
+        if let ImageState::Loaded(ImageStateLoaded { masks, .. }) = &mut self.image_state {
+            if let Some(region) = self.tools.drag_stopped(cursor_image_pos, &response, ctx) {
+                masks.clear_region(region);
+            } else if response.clicked() {
+                masks.clear_region([[cursor_image_pos.0, cursor_image_pos.1]; 2]);
+            }
         }
     }
 }
